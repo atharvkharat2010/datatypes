85342ba883372118a069781ab4f8797fe2bfeefd
atharvkharat2010 <atharvabk1999@gmail.com>
2025-02-02 21:15:34 +0530

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,18 @@
+name ="kunal"
+age = 16
+is_student = True
+weight = 38.5
+print("Name: ",name)
+print("data type of name is: ",type(name))
+print("Age: ",age)
+print("data type of age is: ",type(age))
+print("is_student",is_student)
+print("data type of is_student: ",type(is_student))
+print("Weight: ",weight)
+print("data type of weight is: ",type(weight))
+print("\n after type casting......")
+age=str(age)
+print(age)
+print("data type of age is: ",type(age))
+weight=str(weight)
+print("data type of age is: ",type(weight))

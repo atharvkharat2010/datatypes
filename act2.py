eb6ec729dfbdd9ca64a8c554251eebf3603e46e6
atharvkharat2010 <atharvabk1999@gmail.com>
2025-02-02 21:31:49 +0530

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,18 @@
+num1=25
+num2=4
+print("Number 1",num1)
+print("Number 2",num2)
+print("addition",num1+num2)
+print("subtraction",num1-num2)
+print("multiplication",num1*num2)
+print("division",num1/num2)
+print("floor division",num1//num2)
+print("modular",num1%num2)
+print("square",num1**2)
+print("square root",num2**0.5)
+print("equal?",num1==num2)
+print("not equal?",num1!=num2)
+print("num1 greater?",num1>num2)
+print("num2 greater?",num2>num1)
+result=num1/2+num2*4
+print()
